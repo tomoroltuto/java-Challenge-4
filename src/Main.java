e1e7c6165520883349cd399d337e34bf1a3350c4
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-08-04 00:18:11 +0900

    first commit

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,22 @@ public class Main {
 		//tohoku regionsのリストを作成
 		List<String> tohoku_regions = List.of ("aomori", "iwate", "akita", "miyagi", "yamagata","fukushima");
 		
-		//mが入っているリストを全出力
+		//リストが特定の要素を含むかどうか判定して出力
 		List<String> result = tohoku_regions.stream().filter(region -> region.contains("m")).collect(Collectors.toList());
 		System.out.println(result);
 		System.out.println("");
 		
-		//最初の文字aが始まる単語の数を出力
+		//指定した文字列の始まりの文字列を判定して出力
 		long count = tohoku_regions.stream().filter(region -> region.startsWith("a")).count();
 		System.out.println(count);
 		System.out.println("");
 		
-		//小文字を大文字にする
+		//小文字を大文字にして出力
 		tohoku_regions.stream().map(region -> region.toUpperCase()).forEach(region -> System.out.println(region));
 		System.out.println("");
 		
-		//文字列が一致するかどうかを出力
+		//文字列が一致するかどうかを判定して出力
 		boolean Miyagi = tohoku_regions.stream().anyMatch(region -> region.equals("miyagi"));
 		System.out.println(Miyagi);
 	}
-}
+} 
